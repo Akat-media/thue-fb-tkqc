274fcca7f1cf6e7e6d5d7f1f483b7ee75c39f637
khanhpd <duykhanh141197@gmail.com>
2025-06-07 11:04:54 +0700

    update

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -415,29 +415,29 @@ const HomePage: React.FC = () => {
               <Bell className="w-4 h-4 text-yellow-300" />
               <span className="font-medium font-sans">AKA Media</span>
             </div>
-            <div className="flex items-center gap-6 max-md:hidden">
-              <button
-                type="button"
-                className="inline-flex items-center gap-1 rounded-full bg-gray-950/2 px-2 py-1 inset-ring inset-ring-gray-950/8 dark:bg-white/5 dark:inset-ring-white/2"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 16 16"
-                  className="-ml-0.5 size-4 fill-gray-600 dark:fill-gray-500"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
-                    clipRule="evenodd"
-                  ></path>
-                </svg>
-                <kbd className="font-sans text-xs/4 text-gray-500 not-[.os-macos_&]:block dark:text-gray-400">
-                  Ctrl K
-                </kbd>
-              </button>
-              <Settings className="w-5 h-5 text-gray-600 cursor-pointer" />
-              <User className="w-5 h-5 text-gray-600 cursor-pointer" />
-            </div>
+            {/*<div className="flex items-center gap-6 max-md:hidden">*/}
+            {/*  <button*/}
+            {/*    type="button"*/}
+            {/*    className="inline-flex items-center gap-1 rounded-full bg-gray-950/2 px-2 py-1 inset-ring inset-ring-gray-950/8 dark:bg-white/5 dark:inset-ring-white/2"*/}
+            {/*  >*/}
+            {/*    <svg*/}
+            {/*      xmlns="http://www.w3.org/2000/svg"*/}
+            {/*      viewBox="0 0 16 16"*/}
+            {/*      className="-ml-0.5 size-4 fill-gray-600 dark:fill-gray-500"*/}
+            {/*    >*/}
+            {/*      <path*/}
+            {/*        fillRule="evenodd"*/}
+            {/*        d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"*/}
+            {/*        clipRule="evenodd"*/}
+            {/*      ></path>*/}
+            {/*    </svg>*/}
+            {/*    <kbd className="font-sans text-xs/4 text-gray-500 not-[.os-macos_&]:block dark:text-gray-400">*/}
+            {/*      Ctrl K*/}
+            {/*    </kbd>*/}
+            {/*  </button>*/}
+            {/*  <Settings className="w-5 h-5 text-gray-600 cursor-pointer" />*/}
+            {/*  <User className="w-5 h-5 text-gray-600 cursor-pointer" />*/}
+            {/*</div>*/}
           </div>
 
           <div className="flex flex-1 flex-col">
--- src/pages/admin/account/Account.tsx
+++ src/pages/admin/account/Account.tsx
@@ -11,29 +11,29 @@ const Account = () => {
                     <Bell className="w-4 h-4 text-yellow-300" />
                     <span className="font-medium font-sans text-sm sm:text-base">AKA Media</span>
                 </div>
-                <div className="flex items-center gap-3 sm:gap-6">
-                    <button
-                        type="button"
-                        className="inline-flex items-center gap-1 rounded-full bg-gray-950/2 px-2 py-1 inset-ring inset-ring-gray-950/8 dark:bg-white/5 dark:inset-ring-white/2"
-                    >
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 16 16"
-                            className="-ml-0.5 size-4 fill-gray-600 dark:fill-gray-500"
-                        >
-                            <path
-                                fillRule="evenodd"
-                                d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
-                                clipRule="evenodd"
-                            ></path>
-                        </svg>
-                        <kbd className="font-sans text-xs text-gray-500 not-[.os-macos_&]:block dark:text-gray-400 hidden sm:block">
-                            Ctrl K
-                        </kbd>
-                    </button>
-                    <Settings className="w-5 h-5 text-gray-600 cursor-pointer sm:w-6 sm:h-6" />
-                    <User className="w-5 h-5 text-gray-600 cursor-pointer sm:w-6 sm:h-6" />
-                </div>
+                {/*<div className="flex items-center gap-3 sm:gap-6">*/}
+                {/*    <button*/}
+                {/*        type="button"*/}
+                {/*        className="inline-flex items-center gap-1 rounded-full bg-gray-950/2 px-2 py-1 inset-ring inset-ring-gray-950/8 dark:bg-white/5 dark:inset-ring-white/2"*/}
+                {/*    >*/}
+                {/*        <svg*/}
+                {/*            xmlns="http://www.w3.org/2000/svg"*/}
+                {/*            viewBox="0 0 16 16"*/}
+                {/*            className="-ml-0.5 size-4 fill-gray-600 dark:fill-gray-500"*/}
+                {/*        >*/}
+                {/*            <path*/}
+                {/*                fillRule="evenodd"*/}
+                {/*                d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"*/}
+                {/*                clipRule="evenodd"*/}
+                {/*            ></path>*/}
+                {/*        </svg>*/}
+                {/*        <kbd className="font-sans text-xs text-gray-500 not-[.os-macos_&]:block dark:text-gray-400 hidden sm:block">*/}
+                {/*            Ctrl K*/}
+                {/*        </kbd>*/}
+                {/*    </button>*/}
+                {/*    <Settings className="w-5 h-5 text-gray-600 cursor-pointer sm:w-6 sm:h-6" />*/}
+                {/*    <User className="w-5 h-5 text-gray-600 cursor-pointer sm:w-6 sm:h-6" />*/}
+                {/*</div>*/}
             </div>
 
             {/* Main Content */}
