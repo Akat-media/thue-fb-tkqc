0e51d476af8b86e430bc482d7e51d75ba396fde9
nguyenthanhdat <nguyend@digitalplatformer.co.j>
2025-06-24 16:55:07 +0700

    ud-jenkins

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
-import { defineConfig, loadEnv } from "vite";
-import react from "@vitejs/plugin-react"; // hoặc vue nếu bạn dùng Vue
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react'; // hoặc vue nếu bạn dùng Vue
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
+  const env = loadEnv(mode, process.cwd(), '');
 
   return {
     plugins: [react()],
     define: {
-      "process.env": env,
+      'process.env': env,
+    },
+    server: {
+      host: '0.0.0.0',
+      port: 5173,
     },
   };
 });
