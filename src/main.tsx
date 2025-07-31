53dcbd965824273cc275d200fe0f1893a2287b6b
khanhpd <duykhanh141197@gmail.com>
2025-07-31 15:51:31 +0700

    admin advertisement

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import './index.css';
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
 import './i18n';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 createRoot(document.getElementById('root')!).render(
   <>
